a628ac51db0ce28559ce4b4b16c6040d90982203
FortesFortunaAdiuvat <gg@Guss-MacBook-Pro.local>
2020-08-13 21:20:18 -0600

    Updated bot handler with CLI arguments. Added top level requirements file.

--- commsFalcon.py
+++ commsFalcon.py
@@ -1,38 +1,106 @@
 import requests
+import argparse
 
 from TwitterFollowBot.TwitterFollowBot import TwitterBot
 
-my_bot = TwitterBot('')
+my_bot = TwitterBot('config_weshawes9000.txt')
 
-syncOption = input('Sync Account Info? y/n')
-if syncOption == 'y':
-    print('Syncing Account Info To Local Cache for Twitter Handle ')
-    my_bot.sync_follows()
-else:
-    print('Skipping Account Info Sync.')
+def getHandle(tweeter_id):
+    headers = {
+        'origin':'https://tweeterid.com'
+    }
+    res = requests.post('https://tweeterid.com/ajax.php', data={'input':f'{tweeter_id}'}, headers=headers)
+    print(res.content.decode('utf-8'))
+    return res.content.decode('utf-8')
 
-follow_followers_option = input('Follow back your followers? y/n')
-if follow_followers_option == 'y':
-    print('Following back current Followers...')
+def follow_followers():
     my_bot.auto_follow_followers()
-else:
-    print('Skipping Follow Back Followers.')
+    return
 
-follow_followers_of_user_option = input('Follow users of a specific user? y/n')
-if follow_followers_of_user_option == 'y':
-    user_to_follow = input('Enter User Twitter Handle, without the "@": ')
+def sync_info():
+    my_bot.sync_follows()
+    return
+
+def follow_users_followers(user_to_follow):
     my_bot.auto_follow_followers_of_user(user_to_follow, count=1000)
-else:
-    print('Skipping Follow Followers of User.')
+    return
 
-unfollow_nonfollowers_option = input('Unfollow Non-Followers? y/n')
-if unfollow_nonfollowers_option == 'y':
-    print('Unfollowing Non-Followers...')
+def unfollow_nonfollowers():
     my_bot.auto_unfollow_nonfollowers()
-else:
-    print('Skipping Unfollowing Non-Followers')
+    return
+
+def followPrompts():
+    syncOption = input('Sync Account Info (y/n)? ')
+    if syncOption == 'y':
+        print('Syncing Account Info To Local Cache for Twitter Handle ')
+        my_bot.sync_follows()
+    else:
+        print('Skipping Account Info Sync.')
+
+    follow_followers_option = input('Follow back your followers (y/n)? ')
+    if follow_followers_option == 'y':
+        print('Following back current Followers...')
+        my_bot.auto_follow_followers()
+    else:
+        print('Skipping Follow Back Followers.')
+
+    follow_followers_of_user_option = input('Follow users of a specific user (y/n)? ')
+    if follow_followers_of_user_option == 'y':
+        user_to_follow = input('Enter User Twitter Handle, without the "@": ')
+        my_bot.auto_follow_followers_of_user(user_to_follow, count=1000)
+    else:
+        print('Skipping Follow Followers of User.')
+
+    unfollow_nonfollowers_option = input('Unfollow Non-Followers (y/n)? ')
+    if unfollow_nonfollowers_option == 'y':
+        print('Unfollowing Non-Followers...')
+        my_bot.auto_unfollow_nonfollowers()
+    else:
+        print('Skipping Unfollowing Non-Followers')
 
 
-print('here')
+    print('Done.')
 
+    return
+
+#create argument parser
+my_parser = argparse.ArgumentParser(description='Display options for CommsFalcon Twitter CLI')
+
+# CLI arguments
+user_list = []; tweeter_id_list = [] 
+my_parser.add_argument('-s','--syncInfo', action='store_true',help='Needed to save on API rate limits and faster analysis. Creates local cache of your followers, your following.')
+my_parser.add_argument('-f','--followFollowers', action='store_true', help='Follow Back Your Followers.')
+my_parser.add_argument('-u','--followUsersFollowers', action='store',type=str,metavar=user_list, nargs='+',help='Follow The Followers of a specified user. Do not include @ symbol. Multiple users can be given spearated by a space.')
+my_parser.add_argument('-r','--unfollowNonFollowers', action='store_true', help='Remove/Unfollow Users You Follow Who Do Not Follow You Back.')
+my_parser.add_argument('-c','--convertTweeterID', action='store', type=int, metavar=tweeter_id_list, nargs='+', help='Given unique TweeterID, returns users Handle')
+
+# parse CLI arguments at run time
+args = my_parser.parse_args()
+
+if args.syncInfo:
+    print('syncinfo...')
+    sync_info()
+    print('Done.')
+
+if args.followFollowers:
+    print('Following Back Your Followers...')
+    follow_followers()
+    print('Done.')
+
+if args.followUsersFollowers:
+    print('Following User(s) Followers....')
+    for user in user_list:
+        follow_users_followers(user)
+    print('Done.')
+
+if args.unfollowNonFollowers:
+    print('Unfollowing Users Who Dont Follow You...')
+    unfollow_nonfollowers()
+    print('Done.')
+
+if args.convertTweeterID:
+    print('Converting TweeterID(s)...')
+    for tweeter_id in tweeter_id_list:
+        getHandle(tweeter_id)
+    print('Done.')
 

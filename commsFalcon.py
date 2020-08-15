2e5fdcbb4260cdf8884721472ce3bc2d2aa22f86
FortesFortunaAdiuvat <gg@Guss-MacBook-Pro.local>
2020-08-14 22:13:07 -0600

    Now creates new_followers and lost_followers files upon sync.

--- commsFalcon.py
+++ commsFalcon.py
@@ -16,14 +16,44 @@ def getHandle(tweeter_id):
 def follow_followers():
     my_bot.auto_follow_followers()
     return
+    
 
 def sync_info():
+    previous_followers = []
+    with open('followers.txt', 'r') as f:
+        for line in f:
+            previous_followers.append(line.replace('\n',''))
+
+    # for follower in previous_followers:
+    #     print(follower)
     my_bot.sync_follows()
+    current_followers = []
+    with open('followers.txt','r') as f:
+        for line in f:
+            current_followers.append(line.replace('\n',''))
+    
+    # print(len(previous_followers))
+    # print(len(current_followers))
+    new_followers = list(set(current_followers) - set(previous_followers))
+    lost_followers = list(set(previous_followers) - set(current_followers))
+    with open('new_followers.txt', 'w') as f:
+        for follower in new_followers:
+            f.write(follower+'\n')
+
+    with open('lost_followers.txt', 'w') as f:
+        for follower in lost_followers:
+            f.write(follower+'\n')
+
+    # print(f'{len(new_followers)} New Followers:\n{new_followers}')
+    # print(f'{len(lost_followers)} Lost Followers:\n{lost_followers}')
+    # new_followers = [x for x in set(previous_followers) if x not in set(current_followers)]
+    # print(new_followers)
     return
 
 def follow_users_followers(user_to_follow):
-    my_bot.auto_follow_followers_of_user(user_to_follow, count=1000)
+    my_bot.auto_follow_followers_of_user(user_to_follow, count=10)
     return
+
 
 def unfollow_nonfollowers():
     my_bot.auto_unfollow_nonfollowers()
@@ -73,6 +103,7 @@ my_parser.add_argument('-f','--followFollowers', action='store_true', help='Foll
 my_parser.add_argument('-u','--followUsersFollowers', action='store',type=str,metavar=user_list, nargs='+',help='Follow The Followers of a specified user. Do not include @ symbol. Multiple users can be given spearated by a space.')
 my_parser.add_argument('-r','--unfollowNonFollowers', action='store_true', help='Remove/Unfollow Users You Follow Who Do Not Follow You Back.')
 my_parser.add_argument('-c','--convertTweeterID', action='store', type=int, metavar=tweeter_id_list, nargs='+', help='Given unique TweeterID, returns users Handle')
+my_parser.add_argument('-m','--messageDirect', action='store_true', help='Send direct message to user')
 
 # parse CLI arguments at run time
 args = my_parser.parse_args()
